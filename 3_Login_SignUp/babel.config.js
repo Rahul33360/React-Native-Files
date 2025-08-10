a781aa8c6060df26f65c0da25217450ef05293c1
Rahul33360 <109918304+Rahul33360@users.noreply.github.com>
2025-08-10 23:18:54 +0530

    Restored 3_Login_SignUp folder

---
+++ 3_Login_SignUp/babel.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  presets: ['module:@react-native/babel-preset'],
+};
+
+// babel.config.js
+// module.exports = {
+//   presets: ['module:metro-react-native-babel-preset'],
+//   plugins: ['react-native-reanimated/plugin'],
+// };
